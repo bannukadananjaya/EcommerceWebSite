365376854ad345a77929392e782645d0458a57a6
Subasinghe S.A.B.D. <banukasubasinghe13@gmail.com>
2024-10-17 02:02:16 +0530

    add overflow hidden for item img for howering effect

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom"
 const Item = (props) => {
   return (
-    <div className="p-4 hover:scale-110 transition duration-100">
-         <Link to={`/product/${props.id}`}><img src={props.image} className="" alt="" /></Link>
-         <p>{props.name}</p>
-         <div className="item-prices">
-            <div className="item-prices-new font-medium">
-                ${props.new_price}
-            </div>
-            <div className="item-prices-old text-stone-400 font-light line-through ">
-                ${props.old_price}
-            </div>
-         </div>
+    <div className="p-4">
+      <div className="overflow-hidden">
+      <Link to={`/product/${props.id}`}><img src={props.image} className="hover:scale-110 transition duration-100 overflow-hidden" alt="" /></Link>
+      </div>
+      
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-prices-new font-medium">
+            ${props.new_price}
+        </div>
+        <div className="item-prices-old text-stone-400 font-light line-through ">
+            ${props.old_price}
+        </div>
+      </div>
     </div>
   )
 }
